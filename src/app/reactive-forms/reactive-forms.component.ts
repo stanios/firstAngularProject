ca5b6a90a4827e53225277260f54f736aa4311e9
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-04-04 20:20:55 +0300

    reactive forms

---
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.css'],
+})
+export class ReactiveFormsComponent {
+  form = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    repeatPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
+
+  onSubmit() {
+    if (!this.form.valid) {
+      alert('Errors Detected');
+    }
+    console.log(this.form.value);
+  }
+}
